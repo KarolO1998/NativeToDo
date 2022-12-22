6db003cc48547e9fb545a71430336e7625dfa479
Karol <freespirits1998@gmail.com>
2022-12-22 13:01:45 -0500

    first commit

---
+++ Colors.js
@@ -0,0 +1,9 @@
+export default colors = {
+    black: "#000000",
+    white: "#fffff",
+    green: "#16e441",
+    lightGreen: "#9be314",
+    blue: "#14e3cb",
+    purple: "#bf14e3",
+    orange: "#ff9100"
+}

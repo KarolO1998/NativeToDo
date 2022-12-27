8a8282081411a579b7534a019da62b95936130e0
Karol <freespirits1998@gmail.com>
2022-12-26 20:31:22 -0500

    added static data and changed some colors

---
+++ staticData.js
@@ -0,0 +1,56 @@
+export default staticData = [
+  {
+    name: "Daily Chores",
+    color: "#14d36d",
+    todos: [
+      {
+        title: "Make the Bed",
+        completed: true,
+      },
+      {
+        title: "Do the Dishes",
+        completed: false,
+      },
+      {
+        title: "Start the Landry",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "Work Errands",
+    color: "#bf14e3",
+    todos: [
+      {
+        title: "Reply to Emails",
+        completed: true,
+      },
+      {
+        title: "Get Promotion",
+        completed: false,
+      },
+      {
+        title: "Attending Meeting",
+        completed: true,
+      },
+    ],
+  },
+  {
+    name: "School",
+    color: "##ff9100",
+    todos: [
+      {
+        title: "Attend Lecture",
+        completed: true,
+      },
+      {
+        title: "Start Coding Project",
+        completed: true,
+      },
+      {
+        title: "Finish Quiz",
+        completed: false,
+      },
+    ],
+  },
+];

32183f1bf8d060b93f6bfeb95cef9810e606ef5e
Karol <freespirits1998@gmail.com>
2022-12-26 19:55:58 -0500

    added button and styling for adding todo

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {AntDesign} from "@expo/vector-icons"
 import colors from "./Colors"
 
 export default class App extends React.Component {
@@ -10,10 +11,16 @@ export default class App extends React.Component {
       <View style={{ flexDirection: "row" }}>
         <View style={styles.divider} />
         <Text style={styles.title}>
-          Whats{" "}
-          <Text style={{ fontWeight: "300", color: colors.green }}>To Do</Text>
+          What{" "}
+          <Text style={{ fontWeight: "300", color: colors.green }}>To Do</Text> ?
         </Text>
         <View style={styles.divider} />
+      </View>
+      <View style={{ marginVertical: 40 }}>
+        <TouchableOpacity style={styles.addList}>
+          <AntDesign name="plus" size={16} color={colors.green} />
+        </TouchableOpacity>
+        <Text style={styles.add}>Add ToDo</Text>
       </View>
     </View>
   );
@@ -39,5 +46,19 @@ const styles = StyleSheet.create({
     color: colors.black,
     paddingHorizontal: 14,
 
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.black,
+    borderRadius: 20,
+    alignItems: "center",
+    padding: 16,
+    justifyContent: "center"
+  },
+  add: {
+    color: colors.black,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8,
   }
 });
